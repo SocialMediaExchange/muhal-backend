ca4aa0aa001bdaa4bc8236422262c1906c6df85f
Majd Al-shihabi <majd.al@gmail.com>
2020-09-15 12:17:24 -0400

    change db location to make docker volumes work better

--- muhal/muhal/settings.py
+++ muhal/muhal/settings.py
@@ -89,7 +89,8 @@ WSGI_APPLICATION = 'muhal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'muhal', 'muhal_data.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
