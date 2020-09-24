b8eeb1854dcc7732a54fa19c450f471cd0fd97f0
Majd Al-shihabi <majd.al@gmail.com>
2020-09-23 20:01:53 -0400

    explicitly allowany permissions for reports api view

--- muhal/reports/views.py
+++ muhal/reports/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
 
 from .models import Report
 from .serializers import ReportSerializer
@@ -8,3 +9,4 @@ class ReportCreateViewSet(mixins.CreateModelMixin,
                           viewsets.GenericViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    permission_classes = [AllowAny]
